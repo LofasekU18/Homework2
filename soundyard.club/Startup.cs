9b4c5f4e86ad75c33a5711a69f438c7d0ad65979
Lukáš Strnad <lstrnad13@gmail.com>
2025-03-20 11:21:46 +0100

    start

---
+++ soundyard.club/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(soundyard.club.Startup))]
+namespace soundyard.club
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
